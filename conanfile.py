80441b2e1c9c68821a13027e26659a3f9a39bd4a
smokris <smokris@softpixel.com>
2018-03-31 12:10:25 -0400

    Update LLVM package.

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class CurlConan(ConanFile):
     package_version = '3'
     version = '%s-%s' % (source_version, package_version)
 
-    requires = 'llvm/3.3-1@vuo/stable', \
+    requires = 'llvm/3.3-2@vuo/stable', \
         'openssl/1.0.2n-1@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://curl.haxx.se/'
