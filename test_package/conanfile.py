33874f2ef9a84320f1513fb8dfa06f745fe2bdc3
smokris <smokris@softpixel.com>
2018-03-31 12:05:26 -0400

    Support building on Linux.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -12,12 +12,13 @@ class CurlTestConan(ConanFile):
         self.copy('*', dst='lib', src='lib')
 
     def test(self):
-        self.run('qbs run')
+        self.run('qbs run -f "%s"' % self.source_folder)
 
         # Ensure we only link to system libraries and our own libraries.
         if platform.system() == 'Darwin':
-            self.run('! (otool -L lib/*.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -L lib/libcurl.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -l lib/libcurl.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
-            self.run('! (ldd lib/*.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
+            self.run('! (ldd lib/libcurl.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
