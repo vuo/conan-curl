940a2f885f05180baf1b30b0160fe9b5ddca6fe7
smokris <smokris@softpixel.com>
2018-03-31 17:03:52 -0400

    Allow linking to Linux system libraries.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -19,6 +19,6 @@ class CurlTestConan(ConanFile):
             self.run('! (otool -L lib/libcurl.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
             self.run('! (otool -l lib/libcurl.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
-            self.run('! (ldd lib/libcurl.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
+            self.run('! (ldd lib/libcurl.so | grep -v "^lib/" | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
